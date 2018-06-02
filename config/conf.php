ed885ab3f2804e6e3999ae3fba81ef5bbd951607
SimonGayet <32536263+sygyt@users.noreply.github.com>
2018-06-03 01:24:35 +0200

    tst

--- config/conf.php
+++ config/conf.php
@@ -1,14 +1,14 @@
 <?php
 class conf{
 
-	private $h = getenv('host');
-	private $d = getenv('database');
-	private $u = getenv('username');
-	private $p = getenv('password');
+	 $h = getenv('host');
+	 $d = getenv('database');
+	 $u = getenv('username');
+	 $p = getenv('password');
 	static private $databases = array(
-		'hostname' => $h//'dz8959rne9lumkkw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',//getenv('HOST'),
-		'database' => $d//'gv1zux3l9t7ejpgr',//getenv('DATABASE'),
-		'login' => $u//'forvu5bfe69cewih',//getenv('USERNAME'),
+		'hostname' => $h,//'dz8959rne9lumkkw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',//getenv('HOST'),
+		'database' => $d,//'gv1zux3l9t7ejpgr',//getenv('DATABASE'),
+		'login' => $u,//'forvu5bfe69cewih',//getenv('USERNAME'),
 		'password' => $p//'iadnzx9gnluxwgx8'//getenv('PASSWORD')
 	);
 
