1333b6f7f7f56b5531f0f03770fe044d0fd8a97c
SimonGayet <32536263+sygyt@users.noreply.github.com>
2018-06-03 01:35:29 +0200

    szad

--- config/conf.php
+++ config/conf.php
@@ -1,15 +1,15 @@
 <?php
 class conf{
-
+	/*
 	 $h = getenv('host');
 	 $d = getenv('database');
 	 $u = getenv('username');
-	 $p = getenv('password');
+	 $p = getenv('password');*/
 	static private $databases = array(
-		'hostname' => $h,//'dz8959rne9lumkkw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',//getenv('HOST'),
-		'database' => $d,//'gv1zux3l9t7ejpgr',//getenv('DATABASE'),
-		'login' => $u,//'forvu5bfe69cewih',//getenv('USERNAME'),
-		'password' => $p//'iadnzx9gnluxwgx8'//getenv('PASSWORD')
+		'hostname' => 'dz8959rne9lumkkw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',//getenv('HOST'),
+		'database' => 'gv1zux3l9t7ejpgr',//getenv('DATABASE'),
+		'login' => 'forvu5bfe69cewih',//getenv('USERNAME'),
+		'password' => 'iadnzx9gnluxwgx8'//getenv('PASSWORD')
 	);
 
 	static public function getLogin(){
