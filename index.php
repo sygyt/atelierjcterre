4644d16370349da51b8d02e54b15690defb0fe40
SimonGayet <32536263+sygyt@users.noreply.github.com>
2018-06-03 00:53:28 +0200

    phpinfo

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+	phpinfo();
     $d_s = DIRECTORY_SEPARATOR;
     require_once(__DIR__ . $d_s . "lib" . $d_s . "file.php");
     require_once(File::build_path(array("controller", "routeur.php")));
