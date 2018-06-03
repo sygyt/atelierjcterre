c2f55242d59273593fac31b9faf31cc6f9079d16
SimonGayet <32536263+sygyt@users.noreply.github.com>
2018-06-03 02:50:45 +0200

    cloudinary

--- index.php
+++ index.php
@@ -3,3 +3,7 @@
     $d_s = DIRECTORY_SEPARATOR;
     require_once(__DIR__ . $d_s . "lib" . $d_s . "file.php");
     require_once(File::build_path(array("controller", "routeur.php")));
+    //require_once(File::build_path(array('resources','cloudinary','src','Cloudinary.php');
+	//require_once(File::build_path(array('resources','cloudinary','src','Uploader.php');
+	//require_once(File::build_path(array('resources','cloudinary','src','Api.php');
+
