40258701854b976e875d410317869a4417f3a12e
SimonGayet <32536263+sygyt@users.noreply.github.com>
2018-06-03 02:20:57 +0200

    laaaaast

--- model/model.php
+++ model/model.php
@@ -2,13 +2,13 @@
 require_once(File::build_path(array("config", "conf.php")));
 
 class model{
-  public static $pdo;
+  public  $pdo;
 
-  public static function init(){
-    $login = conf::getLogin();
-    $hostname = conf::getHostname();
-    $database_name = conf::getDatabase();
-    $password = conf::getPassword();
+  static public function init(){
+    $login = getenv('username');
+    $hostname = getenv('host');
+    $database_name = getenv('database');
+    $password = getenv('password');
 
     try{
       self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name", $login, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
